e4beeb9d092f50a0b2cc791e0a2ab30a63e69fad
sankalpsomani17-web <sankalpsomani17@gmail.com>
2026-01-17 19:34:58 +0530

    act.py

---
+++ lesson 2/act1.py
@@ -0,0 +1,16 @@
+#this code will print my name
+print("sankalp")
+#this code will print my age
+print(14)
+#this code will print my school name
+print("DAV PUBLIC SCHOOL")
+#this code will print my grade
+print("8th")
+#this code will print my section
+print("A")
+#this code will print my city
+print("RAIPUR")
+#this code will print my country
+print("INDIA")
+#this code will print my favorite color
+print("BLUE")
