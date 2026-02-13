2c0599582cb800cf74840044517d8285c049ab2a
sankalpsomani17-web <sankalpsomani17@gmail.com>
2026-02-13 17:44:15 +0530

    act4

---
+++ lesson 8/act4.py
@@ -0,0 +1,8 @@
+a=int(input("value 1"))
+b=int(input("value 2"))
+c=int(input("value 3"))
+
+avg=(a+b+c)/3
+print(avg)
+if avg >a and avg  > b and avg > c:
+    print("the avg is grater than 1 , 2, 3")
