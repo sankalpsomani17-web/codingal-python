b6262f17bb60c961ace64e7daa1d5becf5bb5c62
sankalpsomani17-web <sankalpsomani17@gmail.com>
2026-03-31 18:33:40 +0530

    act2

--- lesson 16/act2.py
+++ lesson 16/act2.py
@@ -1,1 +1,11 @@
-d
+def cube(number):
+    return number*number*number
+
+def  by_three(number):
+    if number %3 ==0:
+        return cube (number)
+    else:
+        return False
+    
+print(by_three(9))
+print(by_three(12))
