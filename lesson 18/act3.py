c575061367a7de5e938f0bc0c912aad1fbc1b975
sankalpsomani17-web <sankalpsomani17@gmail.com>
2026-04-06 19:46:04 +0530

    act2

---
+++ lesson 18/act3.py
@@ -0,0 +1,11 @@
+valid=False
+
+while not valid:
+    try:
+        n=int(input("enter a number:"))
+        while n%2==0:
+         print("bye")
+        valid=True
+
+    except ValueError:
+       print("Invalid")
