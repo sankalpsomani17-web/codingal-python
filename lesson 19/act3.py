265dbd905cd2f686c62a05ffb93a234f4fee76aa
sankalpsomani17-web <sankalpsomani17@gmail.com>
2026-04-11 19:37:55 +0530

    act3

---
+++ lesson 19/act3.py
@@ -0,0 +1,16 @@
+import math
+print(math.sqrt(81))
+
+print(math.floor(6.39))
+
+print(math.ceil(6.39))
+
+print(math.fabs(6))
+
+print(math.gcd(18,72))
+
+x,y=2,-3
+print(x,y)
+
+print(math.copysign(x,y))
+
