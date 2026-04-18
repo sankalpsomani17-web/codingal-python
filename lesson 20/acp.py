df0cadc7127b47292c46d23280202ee8e5b415a6
sankalpsomani17-web <sankalpsomani17@gmail.com>
2026-04-18 20:02:52 +0530

    acp

---
+++ lesson 20/acp.py
@@ -0,0 +1,6 @@
+import calendar
+
+# Loop through month numbers 1 to 12
+for month_index in range(1, 13):
+    # Get the month name from the calendar module
+    print(calendar.month_name[month_index])
