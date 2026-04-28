d2b1d8c543a656918c8accb3c8ec0e959ed09aef
sankalpsomani17-web <sankalpsomani17@gmail.com>
2026-04-28 18:49:27 +0530

    act3

---
+++ lesson 23/act3.py
@@ -0,0 +1,10 @@
+country_code = {'India' : '0091',
+'Australia' : '0025',
+'Nepal' : '00977',
+'Canada': '001'}
+
+print("country code for India - ")
+print(country_code.get('India','not found'))
+
+print("country code for japan - ")
+print(country_code.get('japan','not found'))
