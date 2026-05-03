9bb295488f1a5a2d3ba6e4350566c30f5b535f64
sankalpsomani17-web <sankalpsomani17@gmail.com>
2026-05-03 16:25:42 +0530

    acp

--- lesson 25/acp.py
+++ lesson 25/acp.py
@@ -1,11 +1,5 @@
-odd = []
-even = []
-num=int(input("enter a number"))
-for i in range (num):
-    n=int(input("enter a number in list"))
-    if n%2==0:
-        even.append(n)
-    else:
-        odd.append(n)
-print(f"odd list equals{odd}")
-print(f"odd list equals{even}")
+numbers = [1, 2, 3, 4, 5, 6,7,8,9]
+    # Get only the even numbers
+even_numbers = [n for n in numbers if n % 2 == 0]
+    # Result: [2, 4, 6]
+print(even_numbers)
