85d67c4db232bfb5510654be692839f44e34faa8
sankalpsomani17-web <sankalpsomani17@gmail.com>
2026-05-12 16:18:28 +0530

    acp

---
+++ lesson 8/acp.py
@@ -0,0 +1,11 @@
+# Initial values
+a = 10
+b = 20
+c = 30
+
+print(f"Before swap: a = {a}, b = {b}, c = {c}")
+
+# Pythonic way to swap multiple variables
+a, b, c = b, c, a
+
+print(f"After swap:  a = {a}, b = {b}, c = {c}")
