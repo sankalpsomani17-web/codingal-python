891921916d49b77aa6b057ace064b490bc4bf0d0
sankalpsomani17-web <sankalpsomani17@gmail.com>
2026-05-14 18:20:58 +0530

    act1

---
+++ lesson 28/act1.py
@@ -0,0 +1,15 @@
+class IOString():
+
+    def __int__(self):
+        self.str1 = ""
+
+    def get_string(self):
+        self.str1 = input("enter string")
+
+    def print_string(self):
+        print("result is :",self.str1.upper())
+
+str1=IOString()
+
+str1.get_string()
+str1.print_string()
